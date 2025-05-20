6221c25069c880221658f176a4ca14789fbb5671
edward <edwardlfish@gmail.com>
2025-05-20 09:21:55 +0300

    Add custom methods to StudentRepository

--- src/main/java/ait/cohort55/student/dao/StudentRepository.java
+++ src/main/java/ait/cohort55/student/dao/StudentRepository.java
@@ -1,11 +1,18 @@
 package ait.cohort55.student.dao;
 
 import ait.cohort55.student.model.Student;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
     Stream<Student> findByNameIgnoreCase(String name);
+
+    long countByNameInIgnoreCase(Set<String> names);
+
+    @Query("{'scores.?0': {'$gt': ?1}}")
+    Stream<Student> findByExamAndScoreGreaterThan(String exam, int score);
 }
